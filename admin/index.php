13a3ca48f59a60a74bab7d8753aba46c4c0fe6ac
Phạm Hoài Kiệt <123@gmail.com>
2025-05-03 08:31:17 +0700

    change file path

--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- link css -->
-    <link rel="stylesheet" href="./asset/css/reset.css" />
-    <link rel="stylesheet" href="./asset/css/style.css" />
+    <link rel="stylesheet" href="../assets/admin/css/reset.css" />
+    <link rel="stylesheet" href="../assets/admin/css/style.css" />
     <!-- link jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>ADMIN</title>
