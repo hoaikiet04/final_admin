559e1ad4062a7fa283d3a50e669bf210e8344c74
Phạm Hoài Kiệt <123@gmail.com>
2025-05-07 11:38:11 +0700

    final all

--- category.php
+++ category.php
@@ -6,32 +6,22 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- link css -->
-    <link rel="stylesheet" href="./assets/css/reset.css" />
-    <link rel="stylesheet" href="./assets/css/style.css" />
-    <link rel="stylesheet" href="./assets/css/table.css" />
-    <link rel="stylesheet" href="./assets/css/total_section.css" />
+    <link rel="stylesheet" href="./assets/css/reset.css?v=<?= time(); ?>" />
+    <link rel="stylesheet" href="./assets/css/style.css?v=<?= time(); ?>" />
     <!-- link jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <title>Danh sách đại điểm</title>
+    <title>Danh sách địa điểm</title>
   </head>
   <body>
     <navbar class="navbar">
       <h1><a href="./index.php">Danh sách tour hiện có</a></h1>
       <div class="search-bar">
-        <input type="text" placeholder="Tìm kiếm cho ..." />
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            height="20px"
-            margin="0 auto"
-            fill="#28a745"
-          >
-            <path
-              d="M384 432c8.8 0 16-7.2 16-16l0-320c0-8.8-7.2-16-16-16L64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l320 0zm64-16c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320zM224 352c-6.7 0-13-2.8-17.6-7.7l-104-112c-6.5-7-8.2-17.2-4.4-25.9s12.5-14.4 22-14.4l208 0c9.5 0 18.2 5.7 22 14.4s2.1 18.9-4.4 25.9l-104 112c-4.5 4.9-10.9 7.7-17.6 7.7z"
-            />
-          </svg>
-        </button>
+        <div class="inner-user-auth">
+          <a href="../SignInOn/logout.php">
+            <i class="fa-solid fa-right-from-bracket"></i>
+              Đăng xuất
+          </a>
+        </div>
       </div>
     </navbar>
 
